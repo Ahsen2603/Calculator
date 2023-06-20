294bb151df8fdb99172e47dfaf7c87f3f2b2a5c6
Ahsen Ali <ahsen2603@gmail.com>
2023-06-20 21:08:01 +0100

    Loop for eval method

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,17 @@
-let buttons = document.querySelector('.buttons')
+let buttons = document.querySelector('.buttons');
+let btn = buttons.querySelectorAll('span');
+let value = document.getElementById('value');
+
+for(let i=0; i<btn.length; i++){
+    btn[i].addEventListener('click', function(){
+        if(this.innerHTML == "="){
+            value.innerHTML = eval(value.innerHTML);    //I have made use of the eval function to help make my code compact.
+        } else {                                        //This creates ease for myself to code and decouple the programme.
+            if(this.innerHTML=='Clear'){                //It also allows for dynamic operations to occur.
+                value.innerHTML = "";
+            } else {
+                value.innerHTML += this.innerHTML;
+            }
+        }
+    })
+}
