a5b2a10fc0531254329f2001b8d357c08be7987e
Ahsen Ali <ahsen2603@gmail.com>
2023-06-20 21:11:18 +0100

    #

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,7 +2,7 @@ let buttons = document.querySelector('.buttons');
 let btn = buttons.querySelectorAll('span');
 let value = document.getElementById('value');
 
-for(let i=0; i<btn.length; i++){
+for(let i=0; i<btn.length; i++){                        //
     btn[i].addEventListener('click', function(){
         if(this.innerHTML == "="){
             value.innerHTML = eval(value.innerHTML);    //I have made use of the eval function to help make my code compact.
